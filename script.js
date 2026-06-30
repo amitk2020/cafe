e87cecdd317804f76ca0e92bfc824e47ab1e5ae0
Amit <amit.k03377@gmail.com>
2026-06-30 22:12:00 +1200

    Add initial HTML, CSS, and JavaScript for Cafe landing page

---
+++ script.js
@@ -0,0 +1,6 @@
+const reserveButton = document.getElementById('reserveButton');
+
+reserveButton.addEventListener('click', () => {
+    const message = 'Thanks! Your table is reserved. See you soon at Cozy Cafe.';
+    alert(message);
+});
